0eda65b572640a87a0ec91ae833ef2d7b5285c4b
Rohan Kumar <rohandbg2019.rk@gmail.com>
2024-03-17 16:43:44 +0530

    finish it

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBoG-3oQmS4_WFy3IN0Jb60w4--DPd0Wp4",
-    authDomain: "app-3b5fa.firebaseapp.com",
-    projectId: "app-3b5fa",
-    storageBucket: "app-3b5fa.appspot.com",
-    messagingSenderId: "706020348027",
-    appId: "1:706020348027:web:636d50861605faa593d2de",
-    databaseURL:"https://app-3b5fa-default-rtdb.firebaseio.com"
+   
   };
   
   // Initialize Firebase
